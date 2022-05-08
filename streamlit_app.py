706d5d53be5c4b8b81117e118a638b97d213a0b2
s212441 <s212441@student.dtu.dk>
2022-05-08 19:48:03 +0200

    change requirements file

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ df3 = pd.read_csv('_MPA cultural ecosystem services/photo_description_STM.csv', 
 
 # read the fourth file
 # this file includes the list of MPA WDPAID and whether the MPA was included in the analysis of total photo counts or not
-df4 = pd.read_csv('_MPA cultural ecosystem services/MPAincluded_share.csv')
+#df4 = pd.read_csv('_MPA cultural ecosystem services/MPAincluded_share.csv')
 
 # list of all the included WDPAID's
 included = list(df4['WDPAID'][df4['included']==True])
