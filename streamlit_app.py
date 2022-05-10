8bb2392cdf7d709f1adfb23a85b102c06e789d9f
s212441 <s212441@student.dtu.dk>
2022-05-10 15:24:31 +0200

    folium

--- streamlit_app.py
+++ streamlit_app.py
@@ -365,14 +365,14 @@ st.image("UKMPAs.jpg", caption=None, width=None, use_column_width="always", clam
 
 '''
 # Heatmap for all photos
-fig = plt.figure(figsize = (10,5))
+#fig = plt.figure(figsize = (10,5))
 map_hooray = folium.Map(location=[0, 0], 
                         zoom_start = 1)
 heat_df = df3[['longitude', 'latitude']]
 heat_data = [[row['latitude'],row['longitude']] for index, row in heat_df.iterrows()]
 HeatMap(heat_data, radius = 9, blur = 5, min_opacity = 0.2).add_to(map_hooray)
-map_hooray
-st.pyplot(fig)
+#map_hooray
+st.folium(map_hooray)
 st.markdown("### Machine Learning")
 '''
 Text for machine learning
